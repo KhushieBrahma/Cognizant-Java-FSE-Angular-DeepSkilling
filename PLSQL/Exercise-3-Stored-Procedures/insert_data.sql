65fe83e8a6c30971b6cf6609d64bb0ebb8f89bb1
KhushieB <khushiebrahma@gmail.com>
2026-07-02 23:39:45 +0530

    Create insert_data.sql

---
+++ PLSQL/Exercise-3-Stored-Procedures/insert_data.sql
@@ -0,0 +1,22 @@
+INSERT INTO accounts
+VALUES (101,'Rahul','Savings',50000);
+
+INSERT INTO accounts
+VALUES (102,'Priya','Savings',30000);
+
+INSERT INTO accounts
+VALUES (103,'Amit','Current',70000);
+
+INSERT INTO accounts
+VALUES (104,'Neha','Savings',45000);
+
+INSERT INTO employees
+VALUES (1,'John','IT',60000);
+
+INSERT INTO employees
+VALUES (2,'David','IT',70000);
+
+INSERT INTO employees
+VALUES (3,'Sara','HR',50000);
+
+COMMIT;
