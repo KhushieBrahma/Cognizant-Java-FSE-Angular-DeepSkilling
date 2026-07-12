6b3bf2cbcc6aab0baa2cbe00c3e2b292572c0c6d
KhushieB <khushiebrahma@gmail.com>
2026-07-12 15:40:39 +0530

    Create AuthenticationController.java

---
+++ JWT-HandsOn/Create-authentication-service-that-returns-JWT/src/main/java/com.cognizant.jwt_handson/controller/AuthenticationController.java
@@ -0,0 +1,18 @@
+package com.cognizant.jwt_handson.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping("/authenticate")
+    public String authenticate() {
+
+        return """
+        {
+            "token":"dummy-jwt-token"
+        }
+        """;
+    }
+}
